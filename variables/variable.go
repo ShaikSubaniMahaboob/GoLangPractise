820299233537ca68e9c9722d62fb3e82d406f76f
ShaikSubaniMahaboob <subanishaik0320@gmail.com>
2022-06-22 18:15:25 +0530

    Addvariables

---
+++ variables/variable.go
@@ -0,0 +1,26 @@
+package main
+
+//import "time"
+
+func main() {
+	//fmt.Println("subani")
+	println("subani")
+	//println(time.Now())
+
+	var num int
+	println("number is", num)
+
+	var num1 int16 = 88
+	println("number is", num1)
+
+	var decimal float32
+	println("float number is", decimal)
+
+	var ok bool
+	println("bool is", ok)
+	var bt byte
+	println("byte is", bt)
+	var rn rune
+	println("rune is", rn)
+
+}
