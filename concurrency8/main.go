794d1ebdf25d42087ac85a4e73f267d8a0c5f8e4
ShaikSubaniMahaboob <subanishaik0320@gmail.com>
2022-06-29 17:56:11 +0530

    Addconcurrency8

---
+++ concurrency8/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+// something wenr wrong gave to chech properly
+
+func main() {
+	var val chan int
+	fmt.Println("Receive rand int")
+	val = receiveRand() // This retunrs the channel
+
+	go func() {
+		for {
+			v, ok := <-val
+			if ok {
+				fmt.Println(v)
+				close(val)
+				break
+			}
+		}
+	}()
+
+	time.Sleep(time.Millisecond * 1)
+}
+
+func receiveRand() chan int {
+	r := make(chan int, 1)
+	go func() {
+		r <- rand.Intn(1000)
+	}()
+	return r
+}
